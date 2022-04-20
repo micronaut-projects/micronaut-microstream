d705f6aad6bab3ac99b21b09d2168c045a7b377b
Sergio del Amo <sergio.delamo@softamo.com>
2022-04-20 12:18:41 +0200

    use one-microstream-instance not main

--- test-suite-kotlin/src/test/kotlin/io/micronaut/microstream/docs/CustomerControllerTest.kt
+++ test-suite-kotlin/src/test/kotlin/io/micronaut/microstream/docs/CustomerControllerTest.kt
@@ -14,7 +14,7 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.function.Executable
 
-@Property(name = "microstream.storage.main.storage-directory", value = "build/microstream")
+@Property(name = "microstream.storage.one-microstream-instance.storage-directory", value = "build/microstream")
 @MicronautTest
 class CustomerControllerTest {
     @Inject
--- test-suite-kotlin/src/test/kotlin/io/micronaut/microstream/docs/DataRootProvider.kt
+++ test-suite-kotlin/src/test/kotlin/io/micronaut/microstream/docs/DataRootProvider.kt
@@ -4,7 +4,7 @@ import io.micronaut.microstream.conf.RootInstanceProvider
 import jakarta.inject.Named
 import jakarta.inject.Singleton
 
-@Named("main") // <1>
+@Named("one-microstream-instance") // <1>
 @Singleton
 class DataRootProvider : RootInstanceProvider<Data> {
     override fun rootInstance(): Data = Data()
