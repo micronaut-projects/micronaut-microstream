5bc10080cb6d23736b7d3371948a3559086ef820
Tim Yates <tim.yates@gmail.com>
2022-04-27 10:50:22 +0100

    Add a root object for serialization in their rest api

---
+++ microstream-rest/src/main/java/io/micronaut/microstream/rest/RootObject.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2017-2022 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.microstream.rest;
+
+import io.micronaut.core.annotation.NonNull;
+import io.micronaut.serde.annotation.Serdeable;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+/**
+ * This object represents a root object for the Microstream REST api.
+ *
+ * @author Tim Yates
+ * @since 1.0.0
+ */
+@Serdeable
+public class RootObject {
+
+    /**
+     * root name.
+     */
+    @NonNull
+    @NotBlank
+    private String name;
+
+    /**
+     * the root object id.
+     */
+    @NonNull
+    @NotNull
+    private Long objectId;
+
+    /**
+     * @return the name of the root object
+     */
+    @NonNull
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name of the root object
+     */
+    public void setName(@NonNull String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the id of the root object
+     */
+    @NonNull
+    public Long getObjectId() {
+        return objectId;
+    }
+
+    /**
+     * @param objectId the id of the root object
+     */
+    public void setObjectId(@NonNull Long objectId) {
+        this.objectId = objectId;
+    }
+}
---
+++ microstream-rest/src/test/groovy/io/micronaut/microstream/rest/RootObjectSpec.groovy
@@ -0,0 +1,125 @@
+package io.micronaut.microstream.rest
+
+import io.micronaut.context.BeanContext
+import io.micronaut.core.beans.BeanIntrospection
+import io.micronaut.core.type.Argument
+import io.micronaut.serde.ObjectMapper
+import io.micronaut.serde.SerdeIntrospections
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import jakarta.inject.Inject
+import spock.lang.Specification
+
+import javax.validation.Validator
+
+@MicronautTest(startApplication = false)
+class RootObjectSpec extends Specification {
+    @Inject
+    ObjectMapper objectMapper
+
+    @Inject
+    Validator validator
+
+    @Inject
+    BeanContext beanContext
+
+    void "RootObject is annotated with @Serdeable.Deserializable"() {
+        given:
+        SerdeIntrospections serdeIntrospections = beanContext.getBean(SerdeIntrospections)
+
+        when:
+        serdeIntrospections.getDeserializableIntrospection(Argument.of(RootObject))
+
+        then:
+        noExceptionThrown()
+    }
+
+    void "RootObject is annotated with @Serdeable.Serializable"() {
+        given:
+        SerdeIntrospections serdeIntrospections = beanContext.getBean(SerdeIntrospections)
+
+        when:
+        serdeIntrospections.getSerializableIntrospection(Argument.of(RootObject))
+
+        then:
+        noExceptionThrown()
+    }
+
+    void "RootObject is annotated with Introspected"() {
+        when:
+        BeanIntrospection.getIntrospection(RootObject)
+
+        then:
+        noExceptionThrown()
+    }
+
+    void "RootObject::toString() does not throw NPE"() {
+        when:
+        new RootObject().toString()
+
+        then:
+        noExceptionThrown()
+    }
+
+    void "valid RootObject does not trigger any constraint exception"() {
+        when:
+        RootObject el = validRootObject()
+
+        then:
+        validator.validate(el).isEmpty()
+    }
+
+    void "name is required"() {
+        given:
+        RootObject el = validRootObject()
+
+        when:
+        el.name = null
+
+        then:
+        !validator.validate(el).isEmpty()
+    }
+
+    void "objectId is required"() {
+        given:
+        RootObject el = validRootObject()
+
+        when:
+        el.objectId = null
+
+        then:
+        !validator.validate(el).isEmpty()
+    }
+
+    void "values are present in json"() {
+        given:
+        RootObject el = validRootObject()
+
+        when:
+        String json = objectMapper.writeValueAsString(el)
+
+        then:
+        json == '{"name":"rooty","objectId":1}'
+    }
+
+    void "round trip works as expected"() {
+        given:
+        RootObject el = validRootObject()
+
+        when:
+        String json = objectMapper.writeValueAsString(el)
+
+        and:
+        RootObject object = objectMapper.readValue(json, RootObject)
+
+        then:
+        object.name == el.name
+        object.objectId == el.objectId
+    }
+
+    static RootObject validRootObject() {
+        RootObject el = new RootObject()
+        el.name = "rooty"
+        el.objectId = 1
+        el
+    }
+}
