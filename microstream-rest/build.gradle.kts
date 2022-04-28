2c4777039ce4fcdcc7520a5e2df929de6591b492
Tim Yates <tim.yates@gmail.com>
2022-04-28 14:01:27 +0100

    Remove unused dependency

--- microstream-rest/build.gradle.kts
+++ microstream-rest/build.gradle.kts
@@ -9,9 +9,9 @@ dependencies {
 
     implementation(libs.managed.microstream.storage.restservice)
     implementation(libs.micronaut.validation)
-    implementation(libs.micronaut.router)
     implementation(project(":microstream"))
     implementation(libs.micronaut.jackson.databind)
+
     testImplementation(libs.micronaut.http.server.netty)
     testImplementation(libs.micronaut.http.client)
     testImplementation(libs.micronaut.management)
