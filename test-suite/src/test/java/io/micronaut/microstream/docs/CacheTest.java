a60a7003bea84bfd76bb0cc23c53e49e74ed159e
Tim Yates <tim.yates@gmail.com>
2022-05-06 12:27:50 +0100

    Tests and docs

--- test-suite/src/test/java/io/micronaut/microstream/docs/CacheTest.java
+++ test-suite/src/test/java/io/micronaut/microstream/docs/CacheTest.java
@@ -10,27 +10,24 @@ import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class PersistentCacheTest {
+class CacheTest {
 
     @Test
-    void cachePersistsOverRestarts() {
+    void cacheWorksAsExpected() {
         Map<String, Object> config = CollectionUtils.mapOf(
             "storageDirectory", "build/microstream-cache-" + UUID.randomUUID()
         );
         // When we create the app, and use a cached method
-        try (EmbeddedServer server = ApplicationContext.run(EmbeddedServer.class, config, "cachepersist")) {
+        try (EmbeddedServer server = ApplicationContext.run(EmbeddedServer.class, config, "cache")) {
             CounterService counter = server.getApplicationContext().getBean(CounterService.class);
             counter.setCount("Tim", 1337L);
             Long count = counter.currentCount("Tim");
             assertEquals(1337L, count);
-            counter.setCount("Tim", 666L);
-        }
 
-        // Then restarting the app with the same storage location, the value is still cached
-        try (EmbeddedServer server = ApplicationContext.run(EmbeddedServer.class, config, "cachepersist")) {
-            CounterService counter = server.getApplicationContext().getBean(CounterService.class);
-            Long count = counter.currentCount("Tim");
-            assertEquals(666L, count);
+            // Change the store so we check we're seeing a cached value
+            counter.counters.put("Tim", -1L);
+            count = counter.currentCount("Tim");
+            assertEquals(1337L, count);
         }
     }
 }
--- test-suite/src/test/java/io/micronaut/microstream/docs/CounterService.java
+++ test-suite/src/test/java/io/micronaut/microstream/docs/CounterService.java
@@ -1,30 +1,27 @@
 package io.micronaut.microstream.docs;
 
 import io.micronaut.cache.annotation.CacheConfig;
-import io.micronaut.cache.annotation.CacheInvalidate;
 import io.micronaut.cache.annotation.CachePut;
 import io.micronaut.cache.annotation.Cacheable;
-import io.micronaut.context.annotation.Requires;
 import jakarta.inject.Singleton;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 @Singleton
-@CacheConfig("counter")
-@Requires(property = "spec.name", value = "CacheTest")
+@CacheConfig("counter") // <1>
 public class CounterService {
 
     Map<String, Long> counters = new HashMap<>();
 
-    @Cacheable
+    @Cacheable // <2>
     public Long currentCount(String name) {
         return counters.get(name);
     }
 
-    @CacheInvalidate(parameters = {"name"})
-    public void setCount(String name, Long count) {
+    @CachePut(parameters = {"name"}) // <3>
+    public Long setCount(String name, Long count) {
         counters.put(name, count);
+        return count;
     }
 }
