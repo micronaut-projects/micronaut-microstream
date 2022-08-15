acd75d5d8b37115fbef1cef4aa7c553a6b45baad
Dean Wette <wetted@objectcomputing.com>
2022-08-15 16:49:46 -0500

    Prepare for Micronaut 4 (no deprecated code found to remove)

--- microstream/build.gradle.kts
+++ microstream/build.gradle.kts
@@ -2,6 +2,11 @@ plugins {
     id("io.micronaut.build.internal.module")
 }
 
+repositories {
+    maven { setUrl("https://s01.oss.sonatype.org/content/repositories/snapshots/") }
+    mavenCentral()
+}
+
 dependencies {
     compileOnly(mn.micronaut.micrometer.core)
     compileOnly(project(":microstream-annotations"))
