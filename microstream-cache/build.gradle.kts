5476344f11d95b0eaa1305e7afbab6e60c066c80
Tim Yates <tim.yates@gmail.com>
2022-11-04 12:30:10 +0000

    Fix for non-generic ConversionService (#140)

--- microstream-cache/build.gradle.kts
+++ microstream-cache/build.gradle.kts
@@ -5,7 +5,11 @@ plugins {
 dependencies {
     api(project(":microstream"))
     api(libs.managed.microstream.cache)
-    api(mn.micronaut.cache.core)
+    api(mn.micronaut.cache.core) {
+        version {
+            strictly("4.0.0-SNAPSHOT")
+        }
+    }
     testImplementation(mn.micronaut.cache.tck)
     testImplementation(libs.jupiter.api)
 }
