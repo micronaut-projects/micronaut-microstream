8be4b74607f508404405db1213b87860f1d9d3d0
Tim Yates <tim.yates@gmail.com>
2022-11-21 16:11:00 +0000

    Update build to use the new core-bom
    We now need to import the catalogs from any module we consume, they are no longer provided in core.
    
    We cannot pull the platform bom, as this would introduce a circular dependency.

--- microstream-rest/build.gradle.kts
+++ microstream-rest/build.gradle.kts
@@ -4,13 +4,13 @@ plugins {
 
 dependencies {
     annotationProcessor(mn.micronaut.validation)
-    annotationProcessor(mn.micronaut.serde.processor)
+    annotationProcessor(mnSerde.micronaut.serde.processor)
 
     implementation(libs.managed.microstream.storage.restservice)
     implementation(mn.micronaut.validation)
 
     implementation(project(":microstream"))
-    implementation(mn.micronaut.serde.jackson)
+    implementation(mnSerde.micronaut.serde.jackson)
 
     testImplementation(mn.micronaut.http.server.netty)
     testImplementation(mn.micronaut.http.client)
