8bcb832436b7c601f0c02b896eaa2d164d3c0128
Tim Yates <tim.yates@gmail.com>
2022-12-06 12:05:09 +0000

    Stop declaring a version of reactor-test
    And instead rely on the version from micronaut-reactor

--- microstream/build.gradle.kts
+++ microstream/build.gradle.kts
@@ -8,12 +8,13 @@ dependencies {
     api(libs.managed.microstream.storage.embedded.configuration)
 
     compileOnly(mn.micronaut.management)
-    implementation(libs.projectreactor)
+    implementation(mn.reactor)
     implementation(libs.microstream.persistence.binary.jdk8)
     implementation(libs.microstream.persistence.binary.jdk17)
 
+    testImplementation(platform("io.projectreactor:reactor-bom:${mnReactor.versions.reactor.bom.get()}"))
     testImplementation(mnSerde.micronaut.serde.jackson)
-    testImplementation(libs.projectreactor.test)
+    testImplementation(libs.reactor.test)
     testImplementation(mnMicrometer.micronaut.micrometer.core)
     testImplementation(mn.micronaut.management)
     testImplementation(mn.micronaut.http.server.netty)
