554fa3559a92979b697c9171f6382c22fcebd20e
Tim Yates <tim.yates@gmail.com>
2023-10-18 15:44:43 +0100

    Fix java 21 (#279)
    * Update common files
    
    * Fix build
    
    ---------
    
    Co-authored-by: micronaut-build <micronaut-build-account@grails.org>

--- test-suite-utils/build.gradle.kts
+++ test-suite-utils/build.gradle.kts
@@ -1,8 +1,20 @@
 plugins {
     id("java-library")
+    id("io.micronaut.build.internal.common")
     id("io.micronaut.internal.build.microstream-base")
 }
+
 dependencies {
     implementation(libs.managed.microstream.aws.s3)
     implementation(libs.testcontainers)
+}
+
+spotless {
+    java {
+        targetExclude("**/testutils/**")
+    }
+}
+
+tasks.withType<Checkstyle> {
+    enabled = false
 }
