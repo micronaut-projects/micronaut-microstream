e15020105dd7fbabf6311fcb3710abd7c6bc9ed9
Sergio del Amo <sergio.delamo@softamo.com>
2023-11-22 13:24:54 +0100

    remove no existing-eclipsestore-cache

--- eclipsestore-bom/build.gradle.kts
+++ eclipsestore-bom/build.gradle.kts
@@ -2,10 +2,4 @@ plugins {
     id("io.micronaut.internal.build.eclipsestore-base")
     id("io.micronaut.build.internal.bom")
 }
-tasks.named("checkBom").configure { enabled = false }
 tasks.named("checkVersionCatalogCompatibility").configure { enabled = false }
-micronautBuild {
-    binaryCompatibility {
-        enabled.set(false)
-    }
-}
